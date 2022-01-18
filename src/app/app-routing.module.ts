685ef03817933eaa9261f32d8846efcb51938652
Sachin <sachintuli0707@gmail.com>
2022-01-18 14:36:14 +0530

    add routing of 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { TestRoutingComponent } from './test-routing/test-routing.component';
 
 const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
   {
     path: 'test', component: TestRoutingComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestRoutingComponent } from './test-routing/test-routing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestRoutingComponent
+    TestRoutingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
